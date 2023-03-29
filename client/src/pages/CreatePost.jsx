ddeb788e4557b42d7b988264408cae170f0cf1eb
Adel Sliman <adelasliman@gmail.com>
2023-03-29 16:45:23 -0400

    changing server address

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -19,7 +19,7 @@ const CreatePost = () => {
     if (form.prompt){
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        const response = await fetch('https://openasliserver.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -49,7 +49,7 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post',{
+        const response = await fetch('https://openasliserver.onrender.com/api/v1/post',{
           method: 'POST',
           headers: {
               'Content-Type': 'application/json',
